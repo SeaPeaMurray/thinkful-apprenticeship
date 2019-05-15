7dc0c5e05f8994fcccbc39d88e3706d8da30d1f0
Clayton Murray <claypaulmurray@gmail.com>
2019-05-14 19:42:06 -0600

    Completed Euler challenges - began tf and keras notebook.

--- DS_bootcamp/5. Algorithms/5.1.5 Challenge - Proj_Euler/prob_9.py
+++ DS_bootcamp/5. Algorithms/5.1.5 Challenge - Proj_Euler/prob_9.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-l1 = np.arange(1, 100)
-l2 = np.arange(1, 100)
+l1 = np.arange(1, 400)
+l2 = np.arange(1, 400)
+l3 = []
 
-for i, j in zip(*l1):
-	print(i, j)
+for i in l1:
+	for j in l2:
+		hyp = np.sqrt(i**2 + j**2)
+		if hyp == int(hyp):
+			l3.append([i, j, hyp])
+		
+print([np.prod(l) for l in l3 if sum(l) == 1000])
